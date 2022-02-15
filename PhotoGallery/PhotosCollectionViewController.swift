82efa92a8e28e09e7feff8c256f19f4c37604e66
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2022-02-15 22:49:08 +0300

    In progress with filling cell with photos

--- PhotoGallery/PhotosCollectionViewController.swift
+++ PhotoGallery/PhotosCollectionViewController.swift
@@ -9,54 +9,65 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
+    
     let itemsPerRow: CGFloat = 2
     let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return photos.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
         
-        cell.backgroundColor = .black
-        cell.layer.cornerRadius = 20
-    
+        //cell.dogImageView.image?.symbolConfiguration = .unspecified
+        //cell.dogImageView.sizeToFit()
+        
+       // let imageName = photos[indexPath.item]
+       // let image = UIImage(named: imageName)
+       // cell.dogImageView.image = image
+        
+        
+        //cell.dogImageView.clipsToBounds = true
+        //cell.dogImageView.scalesLargeContentImage = true
+         cell.backgroundColor = .black
+        // cell.layer.cornerRadius = 20
+        
         return cell
     }
     
 }
 
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        // set up a two columns of square items
+        
         let paddingWidth = sectionInserts.left * (itemsPerRow + 1)
         let availableWidth = collectionView.frame.width - paddingWidth
         let widthPerItem = availableWidth / itemsPerRow
-
+        
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return sectionInserts
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInserts.left
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInserts.left
     }
